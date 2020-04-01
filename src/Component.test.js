092b9f69a6133fa6afdf08025cf5f6ea991a8526
lesley <lesleybanadzem2017@gmail.com>
2020-04-01 09:52:56 -0700

    Created a mock to mock the store and wrapped the compomnent in  a provider and passed the store in it

--- src/Component.test.js
+++ src/Component.test.js
@@ -1,35 +1,25 @@
+// Configuring enzyme in the test file
 import React from 'react';
-import Enzyme, { mount } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import Enzyme, { mount } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import Component from './Component';
-import { addCount } from './actions';
-import reducer from './reducer';
+//Component
+import Component from "./Component";
+import reducer from "./reducer";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-describe('<Component /> unit test', () => {
-  const getWrapper = (mockStore = createStore(reducer, { count: 0 })) => mount(
+describe("Mount <Component />", () => {
+  //creating a mock for redux store
+  const mockStore = createStore(reducer, {
+    count: 0
+  });
+
+  const getWrapper = () => mount(
     <Provider store={mockStore}>
-      <Component/>
+      <Component />
     </Provider>
   );
-
-  it('should add to count and display the correct # of counts', () => {
-    const wrapper = getWrapper();
-    expect(wrapper.find('h3').text()).toEqual('Count: 0');
-    wrapper.find('button').simulate('click');
-    expect(wrapper.find('h3').text()).toEqual('Count: 1');
-  });
-
-  it('should dispatch the correct action on button click', () => {
-    const mockStore = createStore(reducer, { count: 0 });
-    mockStore.dispatch = jest.fn();
-
-    const wrapper = getWrapper(mockStore);
-    wrapper.find('button').simulate('click');
-    expect(mockStore.dispatch).toHaveBeenCalledWith(addCount());
-  });
 });
